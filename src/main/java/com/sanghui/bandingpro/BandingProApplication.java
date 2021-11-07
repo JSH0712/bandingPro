622187d1b21078e35fe229b2ad0c6c1ce9c4fa4a
정상희 <sstkdgml21@gmail.com>
2021-11-07 20:30:53 +0900

    부트스트랩 커밋

--- src/main/java/com/sanghui/bandingpro/BandingProApplication.java
+++ src/main/java/com/sanghui/bandingpro/BandingProApplication.java
@@ -1,8 +1,11 @@
 package com.sanghui.bandingpro;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BandingProApplication {
@@ -10,5 +13,17 @@ public class BandingProApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BandingProApplication.class, args);
 	}
+
+	@Controller
+	@AllArgsConstructor
+	public class WebController {
+
+		@GetMapping("/")
+		public String main(){
+
+			return "index";
+		}
+	}
+
 
 }
